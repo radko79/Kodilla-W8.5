365af00de668657dfd5a8da59001f028f0e3fef8
Radoslaw Podgorski <radoslaw.podgorski@gmail.com>
2018-01-24 16:12:53 +0000

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+
+	var a = prompt('Enter base length'),
+		h = prompt('Enter triangle height');
+		triangleArea = (a * h)/2;
+		alert('Triangle Area is: ' + triangleArea);
+		console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	//document.getElementById("result").innerHTML = triangleArea;
+	
+
+
+
+
